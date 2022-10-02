8b97f42fe6a1e8ab912a3d94b25547ece4a246c8
Carlos Acosta <113648300+carlosra0345@users.noreply.github.com>
2022-10-02 07:31:58 -0500

    Completed

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -11,6 +11,7 @@
 
 from os import waitid
 import requests
+import cgi
 import CountryFunction
 
 #function which converts degrees in celsius to degrees in farenheit
@@ -18,49 +19,47 @@ def Celsius_to_Fahrenheit(celsius):
     fahrenheit = (celsius * (9/5)) + 32
     return fahrenheit
 
-
+def Weather_Information(destination):
 #1) Experimenting with the OpenWeatherApp API
 #Link for API by city: "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-user_api = "9811532f09e0d432f872927e531e7223"
-destination = input("Enter in (city, country) format: ")
-city = destination.split(",")[0]
-country = destination.split(",")[1]
-country = country[1:]
+    user_api = "9811532f09e0d432f872927e531e7223"
+    city = destination.split(",")[0]
+    country = destination.split(",")[1]
+    country = country[1:]
+
 
 #using the imported CountryFunction.py file to determine the proper country Code
-countryCode = CountryFunction.CountryCodeFunction(country)
+    countryCode = CountryFunction.CountryCodeFunction(country)
 
 
 
 #this is the complete api link including the inputted city location and the key
-complete_api_link = "https://api.openweathermap.org/data/2.5/forecast?q="+city+","+countryCode+"&appid="+user_api #this api link involves city and ISO 3166 CODE
+    complete_api_link = "https://api.openweathermap.org/data/2.5/forecast?q="+city+","+countryCode+"&appid="+user_api #this api link involves city and ISO 3166 CODE
 
 #starting the API request
-weatherResponse = requests.get(complete_api_link)
-specifiedCityData = weatherResponse.json()
+    weatherResponse = requests.get(complete_api_link)
+    specifiedCityData = weatherResponse.json()
 
 
 
 #checking if error is found in api call
-if specifiedCityData['cod'] == "404":
-    print("Error: No existing location in Application")
+    if specifiedCityData['cod'] == "404":
+        print("Error: No existing location in Application")
 #interpretting API call if no error is found
-else:
+    else:
     #Path of the API always begin with the key ['list'], continuing path depends on desired output
     
     #determining temperature of inputted city(it follows the API key path ['list][0]['main]['temp])
-    cityTemperatureCelsius = ((specifiedCityData['list'][0]['main']['temp']) - 273.15)
-    cityTemperatureFahrenheit = Celsius_to_Fahrenheit(cityTemperatureCelsius)
+        cityTemperatureCelsius = ((specifiedCityData['list'][0]['main']['temp']) - 273.15)
+        cityTemperatureFahrenheit = Celsius_to_Fahrenheit(cityTemperatureCelsius)
 
     #finding current description of weather(it follows the API key path ['list']['weather'][0]['description']
-    cityWeatherDescription = specifiedCityData['list'][0]['weather'][0]['description']
+        cityWeatherDescription = specifiedCityData['list'][0]['weather'][0]['description']
     
 
 
-#Outputs of API returning the temperature and weather of inputted location
-print(round(cityTemperatureFahrenheit))
-print(cityWeatherDescription)
-
+#returns of API returning the temperature and weather of inputted location
+    return round(cityTemperatureFahrenheit) , cityWeatherDescription
 
 
 
